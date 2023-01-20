271c13c6e585bea149cb722f6facbc42185412de
skiotto93 <fabio.forenza@hotmail.com>
2023-01-20 18:02:55 +0100

    Aggiunto $fillable Model Comic

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -6,11 +6,11 @@
     <form action="{{ route('comics.store') }}" method="POST"  class="my-4">
     @csrf
         <div class="mb-3">
-            <label for="title" class="form-label">Titolo fumetto*</label>
+            <label for="title" class="form-label">Titolo fumetto *</label>
             <input type="text" class="form-control" id="title" name="title" maxlength="50" required>
         </div>
         <div class="mb-3">
-            <label for="description" class="form-label">Descrizione*</label>
+            <label for="description" class="form-label">Descrizione *</label>
             <textarea class="form-control" id="description" name="description" maxlength="250" required></textarea>
         </div>
         {{-- <div class="mb-3">
@@ -18,19 +18,19 @@
             <input type="image" class="form-control" id="description" name="thumb" maxlength="100" required>
         </div> --}}
         <div class="mb-3">
-            <label for="price" class="form-check-label">Prezzo*</label>
-            <input type="number" class="form-control" id="price" name="price" maxlength="50" required>
+            <label for="price" class="form-check-label">Prezzo</label>
+            <input type="number" class="form-control" id="price" name="price" maxlength="50">
         </div>
         <div class="mb-3">
-            <label for="series" class="form-label">Serie del fumetto*</label>
+            <label for="series" class="form-label">Serie del fumetto *</label>
             <input type="text" class="form-control" id="series" name="series" maxlength="50" required>
         </div>
         <div class="mb-3">
-            <label for="sale_date" class="form-label">Data vendita*</label>
+            <label for="sale_date" class="form-label">Data vendita *</label>
             <input type="date" class="form-control" id="sale_date" name="sale_date" maxlength="50" required>
         </div>
         <div class="mb-3">
-            <label for="type" class="form-label">Tipologia fumetto*</label>
+            <label for="type" class="form-label">Tipologia fumetto *</label>
             <select class="form-select" id="type" name="type" required>
                 <option selected>Seleziona</option>
                 <option value="graphic novel">graphic novel</option>
